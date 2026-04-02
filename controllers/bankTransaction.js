aeb30b0f4349587cea800f5d4ae902af39dcdff7
Midhun-Waywedesign <midhun.km@waywedesign.com>
2026-04-02 22:31:31 +0530

    filter issue solved in bank transaction

--- controllers/bankTransaction.js
+++ controllers/bankTransaction.js
@@ -58,7 +58,7 @@ export const addBankTransaction = TryCatch(async (req, res) => {
     200,
     true,
     "Bank transaction added successfully",
-    bankTransaction
+    bankTransaction,
   );
 });
 
@@ -95,16 +95,15 @@ export const getBankTransactions = TryCatch(async (req, res) => {
 
     let start;
     let end;
-
     if (month && month !== "ALL") {
       const m = Number(month);
 
-      start = new Date(Date.UTC(y, m - 1, 1, 0, 0, 0));
-      end = new Date(Date.UTC(y, m, 0, 23, 59, 59, 999));
+      start = new Date(y, m - 1, 1, 0, 0, 0);
+      end = new Date(y, m, 0, 23, 59, 59, 999);
     } else {
       // Whole year
-      start = new Date(Date.UTC(y, 0, 1, 0, 0, 0));
-      end = new Date(Date.UTC(y, 11, 31, 23, 59, 59, 999));
+      start = new Date(y, 0, 1, 0, 0, 0);
+      end = new Date(y, 11, 31, 23, 59, 59, 999);
     }
 
     periodFilter.transactionDate = {
@@ -228,7 +227,7 @@ export const getBankTransactions = TryCatch(async (req, res) => {
     200,
     true,
     "Bank transactions fetched successfully",
-    responseData
+    responseData,
   );
 });
 
@@ -337,6 +336,6 @@ export const deleteBankTransaction = TryCatch(async (req, res) => {
     200,
     true,
     "Bank transaction deleted successfully",
-    deletedBankTransaction
+    deletedBankTransaction,
   );
 });
